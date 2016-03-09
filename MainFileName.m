efd1d45d12e235d9c2ffd3d4f1ad34b0e1a8c935
SunLoveBing <457285506@qq.com>
2016-03-09 13:54:49 +0800

    获取非文件夹的文件名
    目录下既有文件，又有文件夹，获取非文件夹的文件名，不带后缀，并且输出文件个数和文件名到屏幕。

--- MainFileName.m
+++ MainFileName.m
@@ -4,24 +4,42 @@ clc;
 
 path = 'E:\MatLearn\新建文件夹\test';
 contentInfo = dir(path);
+
+%统计指定目录下有多少个非文件夹的文件
 fileNum = 0;
 for k = 1 : length(contentInfo)
     if strcmp(contentInfo(k).name,'.')||strcmp(contentInfo(k).name,'..')
         continue;
     else
-        fileNum = fileNum + 1;
-    end
-end
-
-fileName = cell(fileNum,1);
-for k = 1 : fileNum
-    fileName{k} = contentInfo(k+2).name;
-    Temp = fileName{k,1};
-    for m = 1 : length(Temp)
-        if strcmp(Temp(m),'.')
-            Temp(m:end) = [];
-            fileName{k} = Temp;
-            break;
+        if contentInfo(k).isdir == 0
+            fileNum = fileNum + 1;
         end
     end
 end
+
+%得到指定目录下非文件夹文件的文件名，不带后缀
+fileName = cell(fileNum,1);
+temp = 1;
+for k = 1 : length(contentInfo)
+    if contentInfo(k).isdir == 1
+        continue;
+    else
+        fileName{temp} = contentInfo(k).name;
+        Temp = fileName{temp,1};
+        for m = 1 : length(Temp)
+            if strcmp(Temp(m),'.')
+                Temp(m:end) = [];
+                fileName{temp} = Temp;
+                temp = temp + 1;
+                break;
+            end
+        end
+    end
+end
+
+fprintf('There are %i files except folders in the directory.',fileNum);
+fprintf('\nTheir names are as follows: ');
+for k = 1 : length(fileName)
+    fprintf('\n%s',fileName{k});
+end
+fprintf('\n');
